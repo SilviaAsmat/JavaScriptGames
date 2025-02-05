69c2c178e833844ca367ccc63a17ee2c332a2243
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-02-05 10:54:46 -0600

    Added initialize function

--- Wordle/wordle.js
+++ Wordle/wordle.js
@@ -6,3 +6,20 @@ var col = 0;// current letter
 
 var gameOver = false;
 var word = "Squid"; 
+
+window.onload = function(){
+    initialize();
+}
+
+function initialize() {
+    //Create board
+    for (let r = 0; r < height; r++) {
+        for (let c = 0; c < width; c++) {
+            // <span id="0-0" class="tile"></span>
+            let tile = document.createElement("span");
+            tile.id = r.toString() + "-" + c.toString(); 
+            tile.classList.add("tile");
+            tile.innerText = "";
+            document.getElementById("board").appendChild(tile); 
+        }
+    }
