1c38dfd301758070af54e780e9e4457ee7bdb684
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-05-23 10:27:03 -0500

    Added renderGame()

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -34,6 +34,26 @@ function startGame() {
   sum = firstCard + secondCard;
   renderGame();
 }
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
 function newCard() {
   if (isAlive === true && hasBlackJack === false) {
     let card = getRandomCard();
@@ -41,3 +61,4 @@ function newCard() {
     cards.push(card);
     renderGame();
   }
+}
