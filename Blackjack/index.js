c66da60b324fa457ba8cd88f180dfd353881383e
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-05-23 10:26:03 -0500

    Initialized variables

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -2,6 +2,19 @@ let player = {
   name: "Silvia",
   chips: 200,
 };
+
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
+
 function getRandomCard() {
   let randomNumber = Math.floor(Math.random() * 13) + 1;
   if (randomNumber > 10) {
